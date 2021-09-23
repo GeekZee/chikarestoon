7ea1dd99ad4dac50e7d67cefb3e7ac1b63b87c1f
TorhamDEV <torhamdev@gmail.com>
2021-09-23 22:47:20 +0330

    add starts models

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,6 +7,7 @@ class UserInfo(models.Model):
     profile = models.ImageField(null=True, blank=True)
     user_ideas = models.ForeignKey('Ideas', on_delete=models.CASCADE, null=True, blank=True)
     user_auth = models.OneToOneField(User, on_delete=models.CASCADE)
+
     def __str__(self) -> str:
         return f"{self.user_auth.username}"
 
@@ -14,7 +15,7 @@ class UserInfo(models.Model):
 class Ideas(models.Model):
     title = models.CharField(max_length=80)
     text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True )
+    date = models.DateTimeField(auto_now_add=True)
     writer = models.ForeignKey(
         'UserInfo',
         on_delete=models.CASCADE,
@@ -22,3 +23,12 @@ class Ideas(models.Model):
 
     def __str__(self) -> str:
         return f"{self.writer.user_auth.username} : {self.title}"
+
+
+class Stars(models.Model):
+    star_number = models.IntegerField()
+    star_idea = models.OneToOneField(Ideas, on_delete=models.CASCADE)
+    star_user = models.OneToOneField(UserInfo, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.star_user.user_auth.username} : {self.star_idea.title} : {self.star_number}"
