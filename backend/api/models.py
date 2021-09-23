3ecc6811930ac42f8761742722d95c7df6d62b83
TorhamDEV <torhamdev@gmail.com>
2021-09-23 16:34:53 +0330

    Add the original model of users to the database model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,27 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class UserInfo(models.Model):
-    name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500, null=True, blank=True)
     profile = models.ImageField(null=True, blank=True)
     user_ideas = models.ForeignKey('Ideas', on_delete=models.CASCADE, null=True, blank=True)
-
+    user_auth = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self) -> str:
-        return f"{self.name}"
+        return f"{self.user_auth.username}"
 
 
 class Ideas(models.Model):
     title = models.CharField(max_length=80)
     text = models.TextField()
-    stars = models.IntegerField(null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    writer = models.OneToOneField(
-        UserInfo,
-        max_length=100,
+    date = models.DateTimeField(auto_now_add=True )
+    writer = models.ForeignKey(
+        'UserInfo',
         on_delete=models.CASCADE,
-        primary_key=True,
     )
 
     def __str__(self) -> str:
-        return f"{self.writer.name} : {self.title}"
+        return f"{self.writer.user_auth.username} : {self.title}"
