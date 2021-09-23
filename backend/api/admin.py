8f373229db72b95b6646a4f1a8adeb740849bb47
TorhamDEV <torhamdev@gmail.com>
2021-09-23 22:47:00 +0330

    Register models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Ideas, UserInfo, Stars
 # Register your models here.
+admin.site.register(Ideas)
+admin.site.register(UserInfo)
+admin.site.register(Stars)
