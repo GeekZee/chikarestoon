e5cc0862ef8f56d7965aa1bcfe05082c25d58da8
onionj <onionj98@gmail.com>
2021-09-24 18:19:07 +0330

    add global premissions IsAuthenticatedOrReadOnly

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'api.permissions.IsStaffOrReadOnly',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',  # ! dj-rest-auth + JWT
