9832b9f19d1d07fdabf08b0fe0f1e8eedfa1db7d
onionj <onionj98@gmail.com>
2021-09-25 00:08:31 +0330

    rename fields

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,17 +5,17 @@ from .serializer import IdeasSerializers
 
 # Create your views here.
 
+
 class IdeasViewSet(ModelViewSet):
-    
+
     serializer_class = IdeasSerializers
     queryset = Ideas.objects.all()
 
-    filterset_fields = ['category_ides', 'author', 'author__username']
-    search_fields = ['title', 'text']
-    ordering_fields = ['id', 'date', 'category_ides', 'author']
-    ordering = ['-date']  # ! default = new to old
-
-
-
-
-
+    filterset_fields = ['category', 'author', 'author__username']
+    search_fields = ['title', 'text', 'slug',
+                     'author__username',
+                     'author__first_name',
+                     'author__last_name',
+                     ]
+    ordering_fields = ['id', 'created', 'updated', 'category', 'author']
+    ordering = ['-created']  # ! default = new to old
