4a0cae73f339349ecfcd5d1de3a15e15c02899d4
onionj <onionj98@gmail.com>
2021-09-25 00:08:11 +0330

    add created and updated field and rename some field

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,27 +4,30 @@ from django.contrib.auth import get_user_model
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=80, unique=True)
-    
+    name = models.CharField(max_length=80, unique=True)
+
     def __str__(self) -> str:
-        return f"{self.category_name}"
+        return f"{self.name}"
 
 
 class UserInfo(models.Model):
     bio = models.TextField(max_length=500, null=True, blank=True)
     profile = models.ImageField(null=True, blank=True)
-    user_auth = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    user_auth = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.user_auth.username}"
 
 
 class Ideas(models.Model):
-    title = models.CharField(max_length=80)
-    text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    category_ides = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=3000)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, null=True, on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.author.username} : {self.title}"
